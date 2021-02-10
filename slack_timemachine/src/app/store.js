03cec122269df161747ba3a4e58a08eca4f0fc7b
Kai Chang <alfred.kai.chang@gmail.com>
2021-02-10 11:39:27 +0800

    [fix] remove unused store

--- slack_timemachine/src/app/store.js
+++ slack_timemachine/src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import conversationReducer from '../features/conversation/conversationSlice'
 import userReducer from '../features/conversation/userSlice'
 import enterReducer from '../features/enter/enterSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     conversation: conversationReducer,
     user: userReducer,
     enter: enterReducer,
